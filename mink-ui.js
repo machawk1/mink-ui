8ed1aef06d3b30184841e40fcc9ff875076998fd
Mat Kelly <me@matkelly.com>
2024-04-23 16:02:23 -0400

    Add sanity check

--- mink-ui.js
+++ mink-ui.js
@@ -1,22 +1,37 @@
 var _el;
 
 function dragOver(e) {
-  if (isBefore(_el, e.target))
-    e.target.parentNode.insertBefore(_el, e.target);
-  else
-    e.target.parentNode.insertBefore(_el, e.target.nextSibling);
+  if (isBefore(_el, e.target)) {
+    e.target.parentNode.insertBefore(_el, e.target)
+  } else {
+    e.target.parentNode.insertBefore(_el, e.target.nextSibling)
+  }
 }
 
 function dragStart(e) {
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/plain", null); // Thanks to bqlou for their comment.
-  _el = e.target;
+  e.dataTransfer.effectAllowed = "move"
+  e.dataTransfer.setData("text/plain", null)
+  _el = e.target
 }
 
-function isBefore(el1, el2) {
+function isBefore (el1, el2) {
   if (el2.parentNode === el1.parentNode)
     for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
       if (cur === el2)
-        return true;
-  return false;
+        return true
+  return false
+}
+
+function sanityCheck () {
+  const minkContainer = document.querySelector('mink-container')
+  const pList = minkContainer.querySelectorAll('precedence-box')
+  for (let i = 0; i < pList.length; i++) {
+    let srcs = pList[i].querySelectorAll('aggregation-source')
+
+    // Identify how many sources are disabled
+    const disabledCount = Array.from(srcs).filter((src) => src.hasAttribute('data-disabled')).length
+    let dString = disabledCount == 0 ? '' : `(${disabledCount} disabled)`
+
+    console.log(`Step ${i+1}: Query ${srcs.length} sources ${dString}`)
+  }
 }
